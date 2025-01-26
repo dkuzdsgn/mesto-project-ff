3209a37871a24de718231ba6b990cc2e84e76165
Daria Kuznetsova <dkuzdsgn@yandex.ru>
2025-01-26 11:01:32 +0100

    fixed mistakes, ready for second review

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,30 @@
 
 // @todo: Функция удаления карточки
 
-const addCard = initialCards.forEach(function (el) {
+const cardsList = document.querySelector(".places__list");
+
+function createCard(cardData, deleteCallback) {
   const cardTemplate = document.querySelector("#card-template").content;
-  const cardsList = document.querySelector(".places__list");
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = el.link;
-  cardElement.querySelector(".card__title").textContent = el.name;
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").alt = `${cardData.name}`;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", function (event) {
-    const card = event.target.closest(".card");
-    deleteCard(card);
+
+  deleteButton.addEventListener("click", function () {
+    deleteCallback(cardElement);
   });
 
-  cardsList.append(cardElement);
-});
+  return cardElement;
+}
 
 function deleteCard(cardElement) {
   cardElement.remove();
 }
+
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, deleteCard);
+  cardsList.append(card);
+});
